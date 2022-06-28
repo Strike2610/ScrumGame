365fad766d969a7fcf5e2079b2362a6b564254da
Tymo <39733374+Strike2610@users.noreply.github.com>
2022-06-28 19:51:22 +0200

    Sprint card creation

---
+++ Assets/Scripts/Sprint.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Sprint", menuName = "Sprint")]
+public class Sprint : ScriptableObject
+{
+	public Color SprintColor;
+	public int sprintNum;
+	public int Value;
+	public int Features;
+}
